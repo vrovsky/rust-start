a1660da1e8e05c49bb32d5ac71f589be51321351
Viachaslau_Rouski <Viachaslau_Rouski@epam.com>
2022-07-11 20:12:05 +0400

    starting to learn something

---
+++ first-app/target/debug/deps/first_app.d
@@ -0,0 +1,5 @@
+C:\Users\Viachaslau_Rouski\Rust\rust-start\first-app\target\debug\deps\first_app.exe: src\main.rs
+
+C:\Users\Viachaslau_Rouski\Rust\rust-start\first-app\target\debug\deps\first_app.d: src\main.rs
+
+src\main.rs:
---
+++ first-app/target/debug/first-app.d
@@ -0,0 +1,1 @@
+C:\Users\Viachaslau_Rouski\Rust\rust-start\first-app\target\debug\first-app.exe: C:\Users\Viachaslau_Rouski\Rust\rust-start\first-app\src\main.rs
