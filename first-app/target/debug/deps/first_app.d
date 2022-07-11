6c79fb13b1cbf9027cfc7a6f18af5cd04bc3a1a1
Viachaslau_Rouski <Viachaslau_Rouski@epam.com>
2022-07-11 21:01:11 +0400

    project changes with deletion of unuesed project files

--- first-app/target/debug/deps/first_app.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Viachaslau_Rouski\Rust\rust-start\first-app\target\debug\deps\first_app.exe: src\main.rs
-
-C:\Users\Viachaslau_Rouski\Rust\rust-start\first-app\target\debug\deps\first_app.d: src\main.rs
-
-src\main.rs:
--- first-app/target/debug/first-app.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Viachaslau_Rouski\Rust\rust-start\first-app\target\debug\first-app.exe: C:\Users\Viachaslau_Rouski\Rust\rust-start\first-app\src\main.rs
