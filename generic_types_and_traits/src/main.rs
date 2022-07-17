07dd43c1132c6331f2d51c0f6f75b4183a02bb15
Viachaslau_Rouski <Viachaslau_Rouski@epam.com>
2022-07-17 20:45:28 +0400

    Generics in functions

--- generic_types_and_traits/src/main.rs
+++ generic_types_and_traits/src/main.rs
@@ -1,3 +1,4 @@
+use core::ops::Add;
 fn main() {
     //Generics - обобщенные типы данных
     //Решает задачу дублирования кода
@@ -21,9 +22,59 @@ fn main() {
     }
 
     impl<T: Copy> Shape1<T> { //
-        pub fn get_x(&self) -> T {
+        pub fn get_x1(&self) -> T {
             self.x1
         }
     }
 
+    //Bigger implementation
+    // pub struct Shape2<T> {
+    //     pub x2: T,
+    //     pub y2: T,
+    // }
+
+    // impl<T: Copy + Add<Output = T>> Shape2<T> { //Применение нескольких трэйтов идет через +
+    //     pub fn sum(&self) -> T {
+    //         self.x2 + self.y2
+    //     }
+    // }
+    pub struct Shape3<T, U>{
+        pub x3: T,
+        pub y3: T,
+        pub z3: U,
+    }
+
+    impl<T: Copy + Add<Output = T>, U: Copy> Shape3<T, U> {
+        pub fn sum(&self) -> T {
+            self.x3 + self.y3
+        }
+        pub fn get_z3(&self) -> U {
+            self.z3
+        }
+    }
+
+    //generics с более упрощенным видом
+    pub struct Shape4<T, U> {
+        pub x4: T,
+        pub y4: T,
+        pub z4: U,
+    }
+    impl<T, U> Shape4<T, U>
+        where
+        T: Copy + Add<Output = T>, //ограничение Trait-ом
+        U: Copy,
+        {
+            pub fn sum(&self) -> T { self.x4 + self.y4 }
+            pub fn get_z(&self) -> U { self.z4}
+        }
+
+    //Generics в функциях
+    pub struct Shape5<T> {
+        pub x5: T,
+        pub y5: T,
+    }
+
+    fn sum<T: Copy + Add<Output = T>>(figure: Shape5<T>) -> T {
+        figure.x5 + figure.y5 
+    }
 }
