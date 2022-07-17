61fbbfa892cb3b02199d1d9df1af1d7efb6d8ac8
Viachaslau_Rouski <Viachaslau_Rouski@epam.com>
2022-07-17 12:58:49 +0400

    option T

--- options_results_error_handling/src/main.rs
+++ options_results_error_handling/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    //Error handling
+    //Обработка ошибок требуется во время компиляции(нельзя скомпилировать)
+    //Типы ошибок: 
+    //Recoverable errors(file not found, network connections is closed, container is empty)
+
+    //Unrecoverable errors(accessing the location out of array bounds and --->
+    //---->Errors that can't be handled by the proggrammer:
+    //        panic! macro(prints failure message, unwinds and cleans up the stack and quit)
+    //Abort on panic! in Cargo.toml: [profile.releas] panic = 'abort'
+    
+    // panic!("Opps, the code is on fire");
+
+    //Recoverable errors - most errors are easy to recover
+    pub enum Option<T>{
+        None,
+        Some(T),
+    }
+    //Usage: initialization of values when you don't want for vatiable to have definition on initilization
+    //Usage: partial functions, whe in 1 function there can be 3, 4 or 5 parameters and can be called with 3 parameters
+    
 }
